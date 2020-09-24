b083525c4cd07003a09fd1a66e40ae39543b1127
Kathleen Yoannon <k.yoannon@gmail.com>
2020-09-23 19:33:36 -0700

    updates

--- script.js
+++ script.js
@@ -1,43 +1,64 @@
-// Assignment Code
-var passwordGenerate = prompt("Please select the length of the password", "enter 8 or 128 characters");
 
-if (passwordGenerate !== "8" && passwordGenerate !== "128") {
-  alert("please enter a valid character");
-} else {
-  var password = document.createElement(passwordGenerate);
-  password.textContent = "Generate password" + passwordGenerate;
-  document.body.appendChild(password);
-}
+var characterTypes = {
+  'upper': 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  'lower': 'abcdefghijklmnopqrstuvwxyz',
+  'number': '0123456789',
+  'special': '!$%^&*()-=+[]{};#:@~,./<>?'
+};
 
-button.onclick=function() {
-// Write password to the #password input
-password (); 
-}
-function password() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-value();
-
-var nextPassword = confirm("Would you like to add another tag?");
-if (nextPassword === true) {
-  var nextpasswordGenerate = prompt("ABCDEFGHIJKLMNOPQRSTUVWXYZ" && passwordGenerate !== "abcdefghijklmnopqrstuvwxyz" && passwordGenerate !== "123456789" && passwordGenerate)
-  if (nextpasswordGenerate !== "ABCDEFGHIJKLMNOPQRSTUVWXYZ" && nextpasswordGenerate !== "abcdefghijklmnopqrstuvwxyz" && nextpasswordGenerate !== "123456789" && nextpasswordGenerate !== "!@#$%^&*?+") {
-    alert("please enter a valid password");
-  } else {
-    var secondpasswordGenerate = document.createElement(secondpasswordGenerate);
-    secondpasswordGenerate.textContent = "This is our second tag via prompts, it's a " + secondpasswordGenerate;
-    document.body.appendChild(secondpasswordGenerate);
+window.onload = function() { 
+  var inputs = document.querySelectorAll('form div input[type=text]');
+  for (var i = 0; i < inputs.length; i++) {
+    var input = inputs[i];
+    var div = input.parentNode;
+    var type = div.id;
+    input.value = characterTypes[type];
+    input.size = 40;
+    var anchor = div.querySelector('a');
+    anchor.onclick = function (input, type) {
+      return function () {
+        input.value = characterTypes[type];
+        return false;
+      };
+    }(input, type);
   }
 }
-var generateBtn;
+
+function _generatePassword(passwordLength, characterBlocks) {
+  var allCharacters = "";
+  for (var i = 0; i < characterBlocks.length; i++) {
+    allCharacters += characterBlocks[i];
+  }
+  var numberCharacters = allCharacters.length;
+  var password = "";
+  for (var i = 1; i <= passwordLength; i++) {
+    password += allCharacters.characterAt(Math.floor(Math.random() * numberCharacters));
+  }
+  return password;
+}
+
+function generatePassword(passwordLength) {
+  var characterBlocks = [];
+  for (id in characterTypes) {
+    var isTicked = document.querySelector('div#' + id + ' input[type=checkbox]').checked;
+    var value = document.querySelector('div#' + id + ' input[type=text]').value;
+    if (isTicked) {
+      characterBlocks.push(value);
+    }
+  }
+  
+  var $length = document.getElementById('length');
+  var passwordLength = parseInt($length.value)
+  
+  var password = _generatePassword(passwordLength, characterBlocks);
+  var $display = document.getElementById('display-password');
+  $display.textContent  = password;
+
+  // var generateBtn;
 // Add event listener to generate button
-generateBtn.addEventListener("click", function(writePassword) {
-  writePassword.preventDefault();
-  generate = generateBtn.value;
-  document.querySelector(".wrapper").style.fontFamily = generate;
-});
+// generateBtn.addEventListener("click", function(writePassword) {
+//   writePassword.preventDefault();
+//   generate = generateBtn.value;
+//   document.querySelector(".wrapper").style.fontFamily = generate;
+// });
+}
